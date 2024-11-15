d52d6cf46a8ded661534da865d7d062ed4457dc1
Alexander Payés <66833788+Payes13@users.noreply.github.com>
2024-11-15 10:28:00 -0500

    Update ChatPage.jsx

--- client/src/routes/chatPage/ChatPage.jsx
+++ client/src/routes/chatPage/ChatPage.jsx
@@ -8,11 +8,9 @@ import './chatPage.css'
 
 const ChatPage = () => {
   const path = useLocation().pathname;
-  // pop() WILL GIVE ME THE LAST ITEM /dashboard/chats/66ac26a7b397542865c8a509
   const chatId = path.split("/").pop();
 
   const { isPending, error, data } = useQuery({
-    // queryKey IS HOW WE IDENTIFY THIS QUERY, IF WE USE A NAME OF ANOTHER QUERY IT WILL OVERWRITE THIS CALL AND FETCH THE DATA BELONGING TO THAT QUERY
     queryKey: ["chat", chatId],
     queryFn: () =>
       fetch(`${import.meta.env.VITE_API_URL}/api/chats/${chatId}`, {
@@ -22,7 +20,6 @@ const ChatPage = () => {
 
   return (
     <div className="chatPage">
-      {/* div className="wrapper" IT'S ADDED SO THAT WE ARE ABLE TO SCROLL */}
       <div className="wrapper">
         <div className="chat">
           {isPending
@@ -39,7 +36,6 @@ const ChatPage = () => {
                       width="400"
                       transformation={[{ height: 300, width: 400 }]}
                       loading="lazy"
-                      // WHILE LOADING IT WILL SHOW A LOW QUALITY VERSION OF THE img
                       lqip={{ active: true, quality: 20 }}
                     />
                   )}
@@ -56,7 +52,6 @@ const ChatPage = () => {
             )
           }
 
-          {/* IT IS ADDED IN ANOTHER component OTHERWISE WHENEVER WE RECEIVE A NEW MESSAGE FROM THE AI IT IS GOING TO RENDER THE ENTIRE ChatPage component */}
           {data && <NewPrompt data={data} />}
         </div>
       </div>
