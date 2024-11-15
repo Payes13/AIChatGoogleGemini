5313350a67e34b0b2d4c505097894412e871b551
Alexander Payés <66833788+Payes13@users.noreply.github.com>
2024-11-15 10:29:15 -0500

    Update HomePage.jsx

--- client/src/routes/homePage/HomePage.jsx
+++ client/src/routes/homePage/HomePage.jsx
@@ -7,31 +7,19 @@ import './homePage.css'
 const HomePage = () => {
   const [typingStatus, setTypingStatus] = useState("human1");
 
-  // const test = async () => {
-  //   await fetch('http://localhost:4000/api/test', {
-  //     credentials: 'include'
-  //   })
-  // }
-
   return (
     <div className="homepage">
-      {/* <img src="/orbital.png" alt="" className='orbital' /> */}
-
       <div className="left">
         <h1>AI CHAT</h1>
         <h2>Supercharge your creativity and productivity</h2>
         <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, magnam sit repellendus voluptas nam eligendi at. Officiis, deserunt velit! Quia a suscipit aliquid iste dolor.</h3>
 
         <Link to='/dashboard'>Get Started</Link>
-
-        {/* <button onClick={test}>TEST BE AUTH</button> */}
       </div>
 
       <div className="right">
         <div className="imgContainer">
-            {/* THIS div IS FOR THE ANIMATED BACKGROUND */}
           <div className="bgContainer">
-            {/* THIS div CONTAINS THE img TO BE ANIMATED */}
             <div className="bg"></div>
           </div>
 
@@ -51,7 +39,6 @@ const HomePage = () => {
             
             <TypeAnimation
                 sequence={[
-                  // Same substring at the start will only be typed out once, initially
                   "Human: We produce food for Mice",
                   2000,
                   () => {
